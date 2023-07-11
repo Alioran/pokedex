1584c1689a14ba58e920d9461a27e032baf62219
Alioran <drakoronknight@gmail.com>
2023-07-10 23:07:55 -0700

    Exercise 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-alert('Hello world');
+let pokemonList = [
+    {name: "Bulbasaur", type: ["Grass","Poison"], height: 2.04},
+    {name: "Charmander", type: ["Fire",null], height: 2.00},
+    {name: "Squirtle", type: ["Water",null], height: 1.08}
+];
 
-let favoriteFood = "boba";
-document.write(favoriteFood);
+let writeOut = "";
+let arrayLength = pokemonList.length;
+console.log(arrayLength);
+
+for (var i=0; i<arrayLength; i++){
+    writeOut = writeOut + (`Name: ${pokemonList[i].name}, Type: ${pokemonList[i].type[0]}`);
+    if (pokemonList[i].type[1] != null){
+        writeOut = writeOut + (` and ${pokemonList[i].type[1]}`);
+    };
+    writeOut = writeOut + (`, Height: ${pokemonList[i].height} <br>`);
+};
+
+document.write(writeOut);
