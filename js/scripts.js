29f7ecf755fe603e6c500db6aa648c603f4b8d75
Alioran <drakoronknight@gmail.com>
2023-07-12 21:37:09 -0700

    Exercise 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,50 @@
-let pokemonList = [
-    {name: "Bulbasaur", type: ["Grass","Poison"], height: 2.04},
-    {name: "Ivysaur", type: ["Grass","Poison"], height: 3.03},
-    {name: "Venausaur", type: ["Grass","Poison"], height: 6.07},
-    {name: "Charmander", type: ["Fire"], height: 2.00},
-    {name: "Charmeleon", type: ["Fire"], height: 3.07},
-    {name: "Charizard", type: ["Fire", "Flying"], height: 5.07},
-    {name: "Squirtle", type: ["Water"], height: 1.08},
-    {name: "Wartortle", type: ["Water"], height: 3.03},
-    {name: "Blastoise", type: ["Water"], height: 5.03}
-];
+let pokemonRepository = (function(){
+    let pokemonList = [];
 
-for (var i=0; i<pokemonList.length; i++){
-    //Pokemon Name and Pokedex No.
-    document.write(`<b>No.${i+1}: ${pokemonList[i].name}</b><br>`);
-
-    //Pokemon Typing
-    document.write(`Type: ${pokemonList[i].type[0]}`);
-    //Only write second type if it exists
-    if (pokemonList[i].type[1] !== undefined){
-        document.write(` and ${pokemonList[i].type[1]}`);
+    function add(pokemon){
+        pokemonList.push(pokemon);
     };
-    //Break after type block
-    document.write(`<br>`);
-
-    //Pokemon Height
-    document.write(`Height: ${pokemonList[i].height}`);
-    //If Pokemon is taller than the average person height of 5.6
-    if (pokemonList[i].height > 5.6){
-        document.write(` (Taller than the average trainer!)`);
+    function getAll(){
+        return pokemonList;
     };
+    return{
+        add: add,
+        getAll: getAll
+    };
+})();
 
-    //Put two breaks after the whole Pokemon stat block
-    document.write(`<br><br>`);
-};
+pokemonRepository.add({name: "Bulbasaur", type: ["Grass","Poison"], height: 2.04});
+pokemonRepository.add({name: "Ivysaur", type: ["Grass","Poison"], height: 3.03});
+pokemonRepository.add({name: "Venausaur", type: ["Grass","Poison"], height: 6.07});
+pokemonRepository.add({name: "Charmander", type: ["Fire"], height: 2.00});
+pokemonRepository.add({name: "Charmeleon", type: ["Fire"], height: 3.07});
+pokemonRepository.add({name: "Charizard", type: ["Fire", "Flying"], height: 5.07});
+pokemonRepository.add({name: "Squirtle", type: ["Water"], height: 1.08});
+pokemonRepository.add({name: "Wartortle", type: ["Water"], height: 3.03});
+pokemonRepository.add({name: "Blastoise", type: ["Water"], height: 5.03});
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(pokedexEntry, index){
+        //Pokemon Name and Pokedex No.
+        document.write(`<b>No.${index+1}: ${pokedexEntry.name}</b><br>`);
+
+        //Pokemon Typing
+        document.write(`Type: ${pokedexEntry.type[0]}`);
+        //Only write second type if it exists
+        if (pokedexEntry.type[1] !== undefined){
+            document.write(` and ${pokedexEntry.type[1]}`);
+        };
+        //Break after type block
+        document.write(`<br>`);
+
+        //Pokemon Height
+        document.write(`Height: ${pokedexEntry.height}`);
+        //If Pokemon is taller than the average person height of 5.6
+        if (pokedexEntry.height > 5.6){
+            document.write(` (Taller than the average trainer!)`);
+        };
+
+        //Put two breaks after the whole Pokemon stat block
+        document.write(`<br><br>`);
+});
