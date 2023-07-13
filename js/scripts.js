778cff5e297563a9c5b58f8b2cf3d0602013152d
Alioran <drakoronknight@gmail.com>
2023-07-13 14:53:10 -0700

    Exercise 1.6

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,68 @@
-let pokemonRepository = (function(){
+//IIFE
+let pokemonRepository = (function () {
     let pokemonList = [];
 
-    function add(pokemon){
+    //Puts the pokemon onto the list of pokemon
+    function add(pokemon) {
         pokemonList.push(pokemon);
     };
-    function getAll(){
+
+    //Gets all entries listed in the pokedex
+    function getAll() {
         return pokemonList;
     };
-    return{
+
+    //Put the item in the pokemonList onto the page
+    function addListItem(pokedexEntry, index) {
+        //Find list in document and set to a variable
+        let list = document.querySelector(".list");
+
+        //Create list item
+        let listItem = document.createElement("li");
+        //Create button in the list item
+        let button = document.createElement("button");
+        //Add text in button for Pokedex Number and Pokemon Name
+        button.innerText = `No.${index+1}: ${pokedexEntry.name}`;
+        //Add class to the button
+        button.classList.add("pokemon-item");
+        //Add click event listener to button
+        button.addEventListener('click', function(){
+            showListDetails(pokedexEntry);
+        });
+
+        //Append button to list item
+        listItem.appendChild(button);
+        //Append list item to the list
+        list.appendChild(listItem);
+    };
+
+    //Check entry
+    function showListDetails(pokedexEntry){
+        console.log(pokedexEntry);
+    };
+
+    return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem : addListItem
     };
 })();
 
-pokemonRepository.add({name: "Bulbasaur", type: ["Grass","Poison"], height: 2.04});
-pokemonRepository.add({name: "Ivysaur", type: ["Grass","Poison"], height: 3.03});
-pokemonRepository.add({name: "Venausaur", type: ["Grass","Poison"], height: 6.07});
-pokemonRepository.add({name: "Charmander", type: ["Fire"], height: 2.00});
-pokemonRepository.add({name: "Charmeleon", type: ["Fire"], height: 3.07});
-pokemonRepository.add({name: "Charizard", type: ["Fire", "Flying"], height: 5.07});
-pokemonRepository.add({name: "Squirtle", type: ["Water"], height: 1.08});
-pokemonRepository.add({name: "Wartortle", type: ["Water"], height: 3.03});
-pokemonRepository.add({name: "Blastoise", type: ["Water"], height: 5.03});
+//Add Items
+pokemonRepository.add({ name: "Bulbasaur", type: ["Grass", "Poison"], height: 2.04 });
+pokemonRepository.add({ name: "Ivysaur", type: ["Grass", "Poison"], height: 3.03 });
+pokemonRepository.add({ name: "Venausaur", type: ["Grass", "Poison"], height: 6.07 });
+pokemonRepository.add({ name: "Charmander", type: ["Fire"], height: 2.00 });
+pokemonRepository.add({ name: "Charmeleon", type: ["Fire"], height: 3.07 });
+pokemonRepository.add({ name: "Charizard", type: ["Fire", "Flying"], height: 5.07 });
+pokemonRepository.add({ name: "Squirtle", type: ["Water"], height: 1.08 });
+pokemonRepository.add({ name: "Wartortle", type: ["Water"], height: 3.03 });
+pokemonRepository.add({ name: "Blastoise", type: ["Water"], height: 5.03 });
 
-console.log(pokemonRepository.getAll());
+//Check list items
+//console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach(function(pokedexEntry, index){
-        //Pokemon Name and Pokedex No.
-        document.write(`<b>No.${index+1}: ${pokedexEntry.name}</b><br>`);
-
-        //Pokemon Typing
-        document.write(`Type: ${pokedexEntry.type[0]}`);
-        //Only write second type if it exists
-        if (pokedexEntry.type[1] !== undefined){
-            document.write(` and ${pokedexEntry.type[1]}`);
-        };
-        //Break after type block
-        document.write(`<br>`);
-
-        //Pokemon Height
-        document.write(`Height: ${pokedexEntry.height}`);
-        //If Pokemon is taller than the average person height of 5.6
-        if (pokedexEntry.height > 5.6){
-            document.write(` (Taller than the average trainer!)`);
-        };
-
-        //Put two breaks after the whole Pokemon stat block
-        document.write(`<br><br>`);
+//Add all items in the repository into the list
+pokemonRepository.getAll().forEach(function (pokedexEntry, index) {
+    pokemonRepository.addListItem(pokedexEntry, index);
 });
