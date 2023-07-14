aa051f6ef736cf5cd135ad31ad07e5ea2dff7a4d
Alioran <drakoronknight@gmail.com>
2023-07-14 03:19:16 -0700

    Exercise 1.7

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //IIFE
 let pokemonRepository = (function () {
     let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     //Puts the pokemon onto the list of pokemon
     function add(pokemon) {
@@ -21,12 +22,13 @@ let pokemonRepository = (function () {
         let listItem = document.createElement("li");
         //Create button in the list item
         let button = document.createElement("button");
+
         //Add text in button for Pokedex Number and Pokemon Name
-        button.innerText = `No.${index+1}: ${pokedexEntry.name}`;
+        button.innerText = `No.${index + 1}: ${pokedexEntry.name}`;
         //Add class to the button
         button.classList.add("pokemon-item");
         //Add click event listener to button
-        button.addEventListener('click', function(){
+        button.addEventListener('click', function () {
             showListDetails(pokedexEntry);
         });
 
@@ -37,32 +39,64 @@ let pokemonRepository = (function () {
     };
 
     //Check entry
-    function showListDetails(pokedexEntry){
-        console.log(pokedexEntry);
+    function showListDetails(pokedexEntry) {
+        loadDetails(pokedexEntry).then(function () {
+            console.log(pokedexEntry);
+          });
     };
+
+    //Load pokemon from API
+    function loadList() {
+        //check if url returns a promise
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {   //if returned, then continue with function
+            //for every pokemon passed through, create array with name and details
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    //Take first letter of name and capitalize it, 
+                    //then add the rest of the letters 
+                    name: item.name.charAt(0).toUpperCase() + item.name.slice(1),
+                    detailsUrl: item.url,
+                    //get sprite of pokemon for the list
+                    //sprite: item.url.sprites.front_default
+                };
+                //add pokemon onto pokemonList
+                add(pokemon);
+            });
+        }).catch(function (e) { //If no promise is resolved, return error
+            console.error(e);
+        })
+    };
+
+    //Load the details of pokemon
+    function loadDetails(pokemon) {
+        let url = pokemon.detailsUrl;  //set url variable to the link listed in detailsUrl
+        return fetch(url).then(function (response) {    //check if it returns a promise
+            return response.json();
+        }).then(function (details) {    //if returned, then continue with function
+            //Find sprite, height, and typing and set it to keys
+            pokemon.imageUrl = details.sprites.front_default;
+            pokemon.height = details.height;
+            pokemon.types = details.types;
+        }).catch(function (e) { //If no promise is resolved, return error
+            console.error(e);
+        });
+    }
 
     return {
         add: add,
         getAll: getAll,
-        addListItem : addListItem
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails
     };
 })();
 
-//Add Items
-pokemonRepository.add({ name: "Bulbasaur", type: ["Grass", "Poison"], height: 2.04 });
-pokemonRepository.add({ name: "Ivysaur", type: ["Grass", "Poison"], height: 3.03 });
-pokemonRepository.add({ name: "Venausaur", type: ["Grass", "Poison"], height: 6.07 });
-pokemonRepository.add({ name: "Charmander", type: ["Fire"], height: 2.00 });
-pokemonRepository.add({ name: "Charmeleon", type: ["Fire"], height: 3.07 });
-pokemonRepository.add({ name: "Charizard", type: ["Fire", "Flying"], height: 5.07 });
-pokemonRepository.add({ name: "Squirtle", type: ["Water"], height: 1.08 });
-pokemonRepository.add({ name: "Wartortle", type: ["Water"], height: 3.03 });
-pokemonRepository.add({ name: "Blastoise", type: ["Water"], height: 5.03 });
-
-//Check list items
-//console.log(pokemonRepository.getAll());
-
-//Add all items in the repository into the list
-pokemonRepository.getAll().forEach(function (pokedexEntry, index) {
-    pokemonRepository.addListItem(pokedexEntry, index);
+//Load data
+pokemonRepository.loadList().then(function () {
+    //Add all items in the repository into the list
+    pokemonRepository.getAll().forEach(function (pokedexEntry, index) {
+        pokemonRepository.addListItem(pokedexEntry, index);
+    });
 });
